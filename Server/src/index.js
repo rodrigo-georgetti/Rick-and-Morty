037438fc3da5480c6d2b594fc122a05837a18061
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-07-10 14:41:25 -0300

    Promises

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,18 +1,21 @@
 const http = require("http");
 const PORT = 3001;
-const data = require("./utils/data");
+const getCharById = require('./controllers/getCharById')
+
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     const { url } = req;
-    if (url.includes("/rickandmorty/character")) {
-      const id = Number(url.split("/").pop());
-      const character = data.find((char) => {
-        return char.id === id;
-      });
-      res.writeHead(200, {'Content-Type':'application/json' });
-      res.end(JSON.stringify(character));
+     if (url.includes("/rickandmorty/character")) {
+        
+      const id = url.split("/").pop();
+    //   const character = data.find((char) => {
+    //     return char.id === id;
+    //   });
+    getCharById(res, id)
+    //   res.writeHead(200, {'Content-Type':'application/json' });
+    //   res.end(JSON.stringify(character));
     }
   })
   .listen(PORT, "localhost");
